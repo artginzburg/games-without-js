17bea4a59105fd375b17f827e30edf41ce0d5abd
Art Ginzburg <ginzburg.arthur@gmail.com>
2023-11-21 00:03:11 +0400

    Add "collect" to lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,5 +3,11 @@ module.exports = {
     upload: {
       target: 'temporary-public-storage',
     },
+    collect: {
+      startServerCommand: 'npm run start',
+      startServerReadyPattern: 'ready on',
+      url: ['http://localhost:3000/', 'http://localhost:3000/memoria'],
+      numberOfRuns: 3,
+    },
   },
 };
