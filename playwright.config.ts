3cc7d6a9972a7d9bc757a76a712ddabc758262c5
Art Ginzburg <ginzburg.arthur@gmail.com>
2023-12-06 00:55:07 +0400

    Allow overriding port for tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,6 +6,8 @@ import { defineConfig, devices } from '@playwright/test';
  */
 // require('dotenv').config();
 
+const port = process.env.PORT ?? 3000;
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -24,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000',
+    baseURL: `http://localhost:${port}`,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -71,7 +73,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
-    url: 'http://localhost:3000',
+    url: `http://localhost:${port}`,
     reuseExistingServer: !process.env.CI,
   },
 });
